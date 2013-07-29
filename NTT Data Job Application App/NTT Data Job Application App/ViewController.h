28f787c8115e319e99d4325916a87b185d8e6d13
Matthias Rabus <rabus.matthias@googlemail.com>
2013-07-29 22:04:50 +0200

    Project created in Xcode

---
+++ NTT Data Job Application App/NTT Data Job Application App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NTT Data Job Application App
+//
+//  Created by Matthias Rabus on 29.07.13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NTT Data Job Application App/NTT Data Job Application App/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NTT Data Job Application App
+//
+//  Created by Matthias Rabus on 29.07.13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
