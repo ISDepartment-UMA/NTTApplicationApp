1bc2325de207ff6a4171b9dbc3d7dc4cde235da4
Ruixing.Yang <ruixing.yang@bradler-gmbh.de>
2013-08-05 17:47:41 +0200

    test commit

---
+++ NTT Data Job Application App/NTT Data Job Application App/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  NTT Data Job Application App
+//
+//  Created by tony yang on 8/5/13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UIView
+
+@end
---
+++ NTT Data Job Application App/NTT Data Job Application App/test.m
@@ -0,0 +1,31 @@
+//
+//  test.m
+//  NTT Data Job Application App
+//
+//  Created by tony yang on 8/5/13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
