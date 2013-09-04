533148c42e3a1036b8a05c8502bba0932e400933
Pavel Kurasov <pavelkurasov@Pavels-Mac-mini.local>
2013-09-04 20:17:28 +0100

    Ad of UI for Job Proposal based on Questions

---
+++ NTT Data Job Application App/NTT Data Job Application App/JobProposalViewController.h
@@ -0,0 +1,13 @@
+//
+//  JobProposalViewController.h
+//  NTT Data Job Application App
+//
+//  Created by Pavel Kurasov on 04.09.13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JobProposalViewController : UIViewController
+
+@end
---
+++ NTT Data Job Application App/NTT Data Job Application App/JobProposalViewController.m
@@ -0,0 +1,38 @@
+//
+//  JobProposalViewController.m
+//  NTT Data Job Application App
+//
+//  Created by Pavel Kurasov on 04.09.13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import "JobProposalViewController.h"
+
+@interface JobProposalViewController ()
+
+@end
+
+@implementation JobProposalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
