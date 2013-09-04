375c18da7cb0f42e09e896192822301add333ba7
Matthias Rabus <rabus.matthias@googlemail.com>
2013-09-04 21:58:01 +0200

    Project directory cleaned up

--- NTT Data Job Application App/nnDataJob/NTT Data Job Application App/connection handling/OSURLHelper.h
+++
@@ -1,9 +0,0 @@
-
-
-#import <Foundation/Foundation.h>
-#import "OSAPIManager.h"
-@interface OSURLHelper : NSObject
-
-+(OSURLHelper*)sharedHelper;
--(NSURL*)getUrl:(OSConnectionType)connectionType;
-@end
