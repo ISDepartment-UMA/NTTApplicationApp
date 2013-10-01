a224d3dd4097e0466ec7d29fbb5ff7c950bcefd4
RuixingYang <ryang@mail.uni-mannheim.de>
2013-10-01 19:56:00 +0200

    applications model created;

---
+++ NTT Job Application Server/NTT Job Application Server/src/model/Applications.java
@@ -0,0 +1,69 @@
+package model;
+
+public class Applications {
+	private String device_id;
+	private String job_ref_no;
+	private String apply_time;
+	private String applicaton_status;
+	private String email;
+	private String first_name;
+	private String last_name;
+	private String address;
+	private String phone_no;
+	public String getDevice_id() {
+		return device_id;
+	}
+	public void setDevice_id(String device_id) {
+		this.device_id = device_id;
+	}
+	public String getJob_ref_no() {
+		return job_ref_no;
+	}
+	public void setJob_ref_no(String job_ref_no) {
+		this.job_ref_no = job_ref_no;
+	}
+	public String getApply_time() {
+		return apply_time;
+	}
+	public void setApply_time(String apply_time) {
+		this.apply_time = apply_time;
+	}
+	public String getApplicaton_status() {
+		return applicaton_status;
+	}
+	public void setApplicaton_status(String applicaton_status) {
+		this.applicaton_status = applicaton_status;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone_no() {
+		return phone_no;
+	}
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+	
+
+}
