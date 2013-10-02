3e91afbfbdf59eca150a09c163f3167e547ced4d
RuixingYang <ryang@mail.uni-mannheim.de>
2013-10-02 15:41:41 +0200

    FAQ model created

---
+++ NTT Job Application Server/NTT Job Application Server/src/model/FAQ.java
@@ -0,0 +1,27 @@
+package model;
+
+public class FAQ {
+	private String num;
+	private String question;
+	private String answer;
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+
+}
