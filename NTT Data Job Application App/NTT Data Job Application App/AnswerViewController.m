38af8f71eaef54735a6d5243e8aff677a3027f2b
hiyavuz <yavuz.hilal1988@googlemail.com>
2013-10-02 16:08:37 -0700

    files for FAQ added & connected to the UI

---
+++ NTT Data Job Application App/NTT Data Job Application App/AnswerViewController.m
@@ -0,0 +1,13 @@
+//
+//  AnswerViewController.m
+//  NTT Data Job Application App
+//
+//  Created by Hilal Yavuz on 10/2/13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import "AnswerViewController.h"
+
+@implementation AnswerViewController
+
+@end
---
+++ NTT Data Job Application App/NTT Data Job Application App/FAQViewController.m
@@ -0,0 +1,13 @@
+//
+//  FAQViewController.m
+//  NTT Data Job Application App
+//
+//  Created by Hilal Yavuz on 10/2/13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import "FAQViewController.h"
+
+@implementation FAQViewController
+
+@end
