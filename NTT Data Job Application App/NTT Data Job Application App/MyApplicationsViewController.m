d1d9c9b3583cc1220d686f0eee295a8c8fd19db1
MTPPavel <pavelk@gmx.de>
2013-10-07 19:12:59 +0100

    Left Swype Deletion for My Applications View enabled

--- NTT Data Job Application App/NTT Data Job Application App/MyApplicationsViewController.m
+++ NTT Data Job Application App/NTT Data Job Application App/MyApplicationsViewController.m
@@ -8,6 +8,52 @@
 
 #import "MyApplicationsViewController.h"
 
+@interface MyApplicationsViewController()
+@property NSMutableArray *data;
+
+@end
 @implementation MyApplicationsViewController
+
+-(void)viewDidLoad{
+    self.data = [[NSMutableArray alloc] initWithObjects:@"Application 1",@" Application 2", @" Application 3", nil];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+ 
+    return [self.data count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"My Application";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    cell.accessoryType= UITableViewCellAccessoryNone;
+    cell.textLabel.font = [UIFont systemFontOfSize:12];
+    
+ 
+        // Configure the cell...
+        cell.userInteractionEnabled = YES;
+        cell.textLabel.text = self.data [indexPath.row];
+        
+    
+  
+    return cell;
+}
+
+
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.data removeObjectAtIndex:indexPath.row];
+        [self.tableView reloadData];
+    }
+    
+}
 
 @end
