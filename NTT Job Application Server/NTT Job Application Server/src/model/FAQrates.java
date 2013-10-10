b8eb7f17219964c670c8038c9cf91299fbfe46db
RuixingYang <ryang@mail.uni-mannheim.de>
2013-10-10 22:36:12 +0200

    FAQrates model created

---
+++ NTT Job Application Server/NTT Job Application Server/src/model/FAQrates.java
@@ -0,0 +1,28 @@
+package model;
+
+public class FAQrates {
+	private String device_id;
+	private String faq_no;
+	private String score;
+	public String getDevice_id() {
+		return device_id;
+	}
+	public void setDevice_id(String device_id) {
+		this.device_id = device_id;
+	}
+	public String getFaq_no() {
+		return faq_no;
+	}
+	public void setFaq_no(String faq_no) {
+		this.faq_no = faq_no;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	
+	
+
+}
