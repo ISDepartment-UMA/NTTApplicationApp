58769834fb8e357feb5319425254cf7680827207
advandy <yunhancheng@Yunhans-Mac.local>
2013-10-18 15:24:51 +0200

    commit dropboxsdk.framework

--- NTT Data Job Application App/NTT Data Job Application App/DropBoxViewController.h
+++ NTT Data Job Application App/NTT Data Job Application App/DropBoxViewController.h
@@ -1,15 +1,15 @@
 //
-//  AppDelegate.h
+//  DropBoxViewController.h
 //  NTT Data Job Application App
 //
-//  Created by Matthias Rabus on 29.07.13.
+//  Created by Yunhan Cheng on 10/18/13.
 //  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import <DropboxSDK/DropboxSDK.h>
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+
+@interface DropBoxViewController : UIViewController
 
 @end
