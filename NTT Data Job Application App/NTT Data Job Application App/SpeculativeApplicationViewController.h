0d200257f093afba72e4239d48109dd11febb6a7
hiyavuz <yavuz.hilal1988@googlemail.com>
2013-10-31 16:37:08 -0700

    Speculative Application View is added via tabbaritem

---
+++ NTT Data Job Application App/NTT Data Job Application App/SpeculativeApplicationViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpeculativeApplicationViewController.h
+//  NTT Data Job Application App
+//
+//  Created by Hilal Yavuz on 10/31/13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpeculativeApplicationViewController : UIViewController
+
+@end
---
+++ NTT Data Job Application App/NTT Data Job Application App/SpeculativeApplicationViewController.m
@@ -0,0 +1,13 @@
+//
+//  SpeculativeApplicationViewController.m
+//  NTT Data Job Application App
+//
+//  Created by Hilal Yavuz on 10/31/13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import "SpeculativeApplicationViewController.h"
+
+@implementation SpeculativeApplicationViewController
+
+@end
