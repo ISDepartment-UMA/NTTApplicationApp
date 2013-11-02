9d8ad223e017f6c7d650d528c0729b080bdfd604
hiyavuz <yavuz.hilal1988@googlemail.com>
2013-11-02 11:55:35 -0700

    UI Element and View Controller to save and show filter sets

---
+++ NTT Data Job Application App/NTT Data Job Application App/MyFilterSetsViewController.m
@@ -0,0 +1,13 @@
+//
+//  MyFilterSetsViewController.m
+//  NTT Data Job Application App
+//
+//  Created by Hilal Yavuz on 11/2/13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import "MyFilterSetsViewController.h"
+
+@implementation MyFilterSetsViewController
+
+@end
