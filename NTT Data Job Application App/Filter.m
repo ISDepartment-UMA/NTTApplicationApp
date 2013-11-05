c8722f1671d1a9893c7a1cf983fb946bb42cdd46
advandy <yunhancheng@Yunhans-Mac.local>
2013-11-05 13:18:30 +0100

    create database for saving filter set

---
+++ NTT Data Job Application App/Filter.m
@@ -0,0 +1,19 @@
+//
+//  Filter.m
+//  NTT Data Job Application App
+//
+//  Created by Yunhan Cheng on 11/5/13.
+//  Copyright (c) 2013 University of Mannheim - NTT Data Team Project. All rights reserved.
+//
+
+#import "Filter.h"
+
+
+@implementation Filter
+
+@dynamic titleFilter;
+@dynamic topicFilter;
+@dynamic locationFilter;
+@dynamic expFilter;
+
+@end
