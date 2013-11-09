a08e68a20a26aef23bd042d7d2f7eab24adc20e8
RuixingYang <ryang@mail.uni-mannheim.de>
2013-11-09 15:32:24 +0100

    FilterSet model created

---
+++ NTT Job Application Server/NTT Job Application Server/src/model/FilterSet.java
@@ -0,0 +1,43 @@
+package model;
+
+public class FilterSet {
+	private String device_id;
+	private String job_title;
+	private String location;
+	private String topic;
+	private String exp;
+	public String getDevice_id() {
+		return device_id;
+	}
+	public void setDevice_id(String device_id) {
+		this.device_id = device_id;
+	}
+	public String getJob_title() {
+		return job_title;
+	}
+	public void setJob_title(String job_title) {
+		this.job_title = job_title;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getTopic() {
+		return topic;
+	}
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	public String getExp() {
+		return exp;
+	}
+	public void setExp(String exp) {
+		this.exp = exp;
+	}
+	 
+	
+	
+
+}
