adbe219c157f10d6062f3b67d5660b2c1e64e07e
RuixingYang <ryang@mail.uni-mannheim.de>
2013-11-10 18:59:23 +0100

    model of speculativeApplication created

---
+++ NTT Job Application Server/NTT Job Application Server/src/model/SpeculativeApplication.java
@@ -0,0 +1,78 @@
+package model;
+
+public class SpeculativeApplication {
+	private String uuid;
+	private String device_id;
+	private String apply_time;
+	private String application_satus;
+	private String email;
+	private String first_name;
+	private String last_name;
+	private String address;
+	private String phone_no;
+	private String dropbox_url;
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public String getDevice_id() {
+		return device_id;
+	}
+	public void setDevice_id(String device_id) {
+		this.device_id = device_id;
+	}
+	public String getApply_time() {
+		return apply_time;
+	}
+	public void setApply_time(String apply_time) {
+		this.apply_time = apply_time;
+	}
+	public String getApplication_satus() {
+		return application_satus;
+	}
+	public void setApplication_satus(String application_satus) {
+		this.application_satus = application_satus;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone_no() {
+		return phone_no;
+	}
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+	public String getDropbox_url() {
+		return dropbox_url;
+	}
+	public void setDropbox_url(String dropbox_url) {
+		this.dropbox_url = dropbox_url;
+	}
+	
+	
+	
+
+}
