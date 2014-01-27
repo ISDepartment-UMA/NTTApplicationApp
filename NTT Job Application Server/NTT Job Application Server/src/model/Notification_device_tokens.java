7f5385cf69b596930d6be4089d5dea60c80a6a91
RuixingYang <ryang@mail.uni-mannheim.de>
2014-01-27 21:02:04 +0800

    Notification_device_tokens created

---
+++ NTT Job Application Server/NTT Job Application Server/src/model/Notification_device_tokens.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Notification_device_tokens {
+	private String device_token;
+
+	public String getDevice_token() {
+		return device_token;
+	}
+
+	public void setDevice_token(String device_token) {
+		this.device_token = device_token;
+	}
+	
+
+}
