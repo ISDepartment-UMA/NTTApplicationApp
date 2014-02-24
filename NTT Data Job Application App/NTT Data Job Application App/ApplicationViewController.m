72cabdab83beee7e7f96de98ce421a9bda694d33
Matthias Rabus <rabus.matthias@googlemail.com>
2014-02-24 19:21:25 +0100

    Integrated LinkedIn library

--- NTT Data Job Application App/NTT Data Job Application App/ApplicationViewController.m
+++ NTT Data Job Application App/NTT Data Job Application App/ApplicationViewController.m
@@ -300,13 +300,43 @@ JVFloatLabeledTextField *phoneField;
 
 - (IBAction)applyViaLinkedIn:(id)sender
 {
+    [[self client] getAuthorizationCode:^(NSString *code) {
+        [[self client] getAccessToken:code success:^(NSDictionary *accessTokenData) {
+            NSString *accessToken = [accessTokenData objectForKey:@"access_token"];
+            [self requestMeWithToken:accessToken];
+        }                   failure:^(NSError *error) {
+            NSLog(@"Quering accessToken failed %@", error);
+        }];
+    }                      cancel:^{
+        NSLog(@"Authorization was cancelled by user");
+    }                     failure:^(NSError *error) {
+        NSLog(@"Authorization failed %@", error);
+    }];
+}
+
+- (void)requestMeWithToken:(NSString *)accessToken{
+    [[self client] getPath:[NSString stringWithFormat:@"https://api.linkedin.com/v1/people/~?oauth2_access_token=%@&format=json", accessToken] parameters:nil
+    success:^(AFHTTPRequestOperation *operation, NSDictionary *result)
+    {
+        //Application logic!
+        NSLog(@"current user %@", result);
+    }
+    failure:^(AFHTTPRequestOperation *operation, NSError *error)
+    {
+        NSLog(@"failed to fetch current user %@", error);
+    }];
+}
+
+#define LINKEDIN_CLIENT_ID @"777rroifzey1nn"
+#define LINKEDIN_CLIENT_SECRET @"hx06ctBGBHwtScwG"
+- (LIALinkedInHttpClient *)client {
     LIALinkedInApplication *application = [LIALinkedInApplication applicationWithRedirectURL:@"http://www.ancientprogramming.com/liaexample"
-                                                                                    clientId:@"clientId"
-                                                                                clientSecret:@"clientSecret"
+                                                                                    clientId:LINKEDIN_CLIENT_ID
+                                                                                clientSecret:LINKEDIN_CLIENT_SECRET
                                                                                        state:@"DCEEFWF45453sdffef424"
-                                                                               grantedAccess:@[@"r_fullprofile", @"r_network"]];
-    [LIALinkedInHttpClient clientForApplication:application presentingViewController:nil];
-    
+                                                                               grantedAccess:@[@"r_basicprofile"]
+                                           ];
+    return [LIALinkedInHttpClient clientForApplication:application presentingViewController:self];
 }
 
 - (void)viewDidLoad
@@ -314,7 +344,6 @@ JVFloatLabeledTextField *phoneField;
     [super viewDidLoad];
     
     [self.view setTintColor:[UIColor orangeColor]];
-    
     UIColor *floatingLabelColor = [UIColor grayColor];
     
     
